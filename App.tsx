13e74b4515c984e6fac009a8332064440de1f5e4
George <georgenico9@gmail.com>
2024-05-30 23:16:13 +0200

    folder structure

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import { config } from "@gluestack-ui/config";
-import { Box, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import {
+  Box,
+  GluestackUIProvider,
+  SafeAreaView,
+  Text,
+} from "@gluestack-ui/themed";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StatusBar, StyleSheet } from "react-native";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <GluestackUIProvider config={config}>
-      <Box width="100%" justifyContent="center" alignItems="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </GluestackUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <GluestackUIProvider config={config}>
+        <SafeAreaView style={styles.container}>
+          <Box justifyContent="center" alignItems="center">
+            <Text>Open up App.js to start working on your app!</Text>
+          </Box>
+        </SafeAreaView>
+      </GluestackUIProvider>
+    </QueryClientProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight,
+  },
+});
