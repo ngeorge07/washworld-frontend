de3224cbd34e8837bff75ab197b1e5c9707a20dc
George <georgenico9@gmail.com>
2024-06-02 22:24:11 +0200

    create sign up slice

---
+++ src/features/auth/authSlice.tsx
@@ -0,0 +1,82 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "../../app/axios";
+import { AppDispatch } from "../../app/store";
+import { User } from "../../types/User";
+
+export interface AuthState {
+  isSignedIn: boolean;
+  user: User | null;
+  status: "initial" | "loading" | "success" | "failure";
+}
+
+type SignUpPayloadType = {
+  fullName: string;
+  email: string;
+  password: string;
+};
+
+const initialState: AuthState = {
+  isSignedIn: false,
+  user: null,
+  status: "initial",
+};
+
+export type SignupResponse = {
+  fullName: string;
+  email: string;
+  id: number;
+  password: string;
+  roles: string[];
+  washCoins: number;
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    initial: (state: AuthState) => {
+      state.status = "initial";
+    },
+    request: (state: AuthState) => {
+      state.status = "loading";
+    },
+    signupSuccess: (state: AuthState) => {
+      state.status = "success";
+    },
+
+    failure: (state: AuthState) => {
+      state.user = null;
+      state.isSignedIn = false;
+      state.status = "failure";
+    },
+  },
+});
+
+export const signUp =
+  (payload: SignUpPayloadType) => async (dispatch: AppDispatch) => {
+    // set the status to loading
+    dispatch(authSlice.actions.request());
+    try {
+      const response = await axios.post<SignupResponse>(
+        "/auth/signup",
+        payload
+      );
+      const user = response.data.id;
+      if (user) {
+        dispatch(authSlice.actions.signupSuccess());
+        return response.data;
+      } else {
+        dispatch(authSlice.actions.failure());
+        throw new Error("Invalid response from server while creating user");
+      }
+    } catch (error) {
+      console.log(error);
+      dispatch(authSlice.actions.failure());
+    } finally {
+      dispatch(authSlice.actions.initial());
+    }
+  };
+
+export const { initial, request, failure, signupSuccess } = authSlice.actions;
+
+export default authSlice.reducer;
---
+++ src/types/User.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id: number;
+  fullName: string;
+  email: string;
+  password: string;
+  role: string;
+  washCoins: number;
+  roles: string[];
+};
